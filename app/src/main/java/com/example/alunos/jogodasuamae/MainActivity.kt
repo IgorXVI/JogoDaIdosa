bcd0809f434afd565a304d9ef8a48347dd9f51e8
Igor Almeida <inazumaseleven04@gmail.com>
2018-09-28 20:31:31 -0300

    Meu ilustre colega.

--- app/src/main/java/com/example/alunos/jogodasuamae/MainActivity.kt
+++ app/src/main/java/com/example/alunos/jogodasuamae/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import java.security.AccessController.getContext
 
 class MainActivity : AppCompatActivity() {
@@ -12,15 +13,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button1.setOnClickListener{
+            vezJogador(button1)
+        }
+        button2.setOnClickListener{
+            vezJogador(button2)
+        }
+        button3.setOnClickListener{
+            vezJogador(button3)
+        }
+        button4.setOnClickListener{
+            vezJogador(button4)
+        }
+        button5.setOnClickListener{
+            vezJogador(button5)
+        }
+        button6.setOnClickListener{
+            vezJogador(button6)
+        }
+        button7.setOnClickListener{
+            vezJogador(button7)
+        }
+        button8.setOnClickListener{
+            vezJogador(button8)
+        }
+        button9.setOnClickListener{
+            vezJogador(button9)
+        }
     }
 
-    fun resetarJogo(){
+    fun resetarJogo(view: View){
         runOnUiThread {
             var btn: Button
             var id: Int
             var nome: String
             for(i in 1..9){
-                nome = "Button{$i}"
+                nome = "button{$i}"
                 id = resources.getIdentifier(nome, "id", this.packageName)
                 btn = findViewById(id)
                 btn.setText(" ")
@@ -46,6 +74,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun vezComputador(){
-
+        
     }
 }
