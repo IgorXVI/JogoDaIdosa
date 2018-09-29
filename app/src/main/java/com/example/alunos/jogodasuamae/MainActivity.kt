0f5faf0f72dc3e0101b43f13522926c1a041c06b
Alunos <inazumaseleven04@gmail.com>
2018-09-28 21:28:02 -0300

    Aula chata.

--- app/src/main/java/com/example/alunos/jogodasuamae/MainActivity.kt
+++ app/src/main/java/com/example/alunos/jogodasuamae/MainActivity.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import java.security.AccessController.getContext
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -93,23 +92,37 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun checkarFim(){
-        var btn1 = pegarBotao(1)
-        var btn2 = pegarBotao(2)
-        var btn3 = pegarBotao(3)
-        var btn4 = pegarBotao(4)
-        var btn5 = pegarBotao(5)
-        var btn6 = pegarBotao(6)
-        var btn7 = pegarBotao(7)
-        var btn8 = pegarBotao(8)
-        var btn9 = pegarBotao(9)
+        var btn1 = pegarBotao(1).text
+        var btn2 = pegarBotao(2).text
+        var btn3 = pegarBotao(3).text
+        var btn4 = pegarBotao(4).text
+        var btn5 = pegarBotao(5).text
+        var btn6 = pegarBotao(6).text
+        var btn7 = pegarBotao(7).text
+        var btn8 = pegarBotao(8).text
+        var btn9 = pegarBotao(9).text
+
     }
 
-    fun checkarLado(txt: String): Boolean{
+    fun travarTudo(){
+        var btn: Button
+        for(i in 1..9){
+            btn = pegarBotao(i)
+            if(btn.text.equals(" ")){
+                btn.setEnabled(false)
+            }
+        }
+    }
+
+    fun checkarLado(txt: String): Int{
         if(txt.equals("X")){
-            return true
+            return 1
+        }
+        else if(txt.equals("O")){
+            return 2
         }
         else{
-            return false
+            return 3
         }
     }
 }
